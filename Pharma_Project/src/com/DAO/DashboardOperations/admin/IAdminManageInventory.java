b8eaaeb88d6dc798b2913fe07e8a18a4c51c1620
DEEPAK <DEEPAK@DESKTOP-SGB4A0M>
2018-03-21 23:14:22 +0530

    randomcommit

--- Pharma_Project/src/com/DAO/DashboardOperations/admin/IAdminManageInventory.java
+++ Pharma_Project/src/com/DAO/DashboardOperations/admin/IAdminManageInventory.java
@@ -1,10 +1,13 @@
 package com.DAO.DashboardOperations.admin;
 
+import com.DAO.Credentials;
+import com.DAO.ProductExpiryPOJO;
 import com.DAO.ProductPOJO;
 
 public interface IAdminManageInventory {
 	
 	public ProductPOJO[] getAllProducts();
+	public boolean addProduct(Credentials auth, ProductExpiryPOJO productInfo);
 	
 
 }
